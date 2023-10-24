60d88cee8af4fc37b358ae58e9a85ae1436ae845
akshay the the great <nandakumarakshay@ufl.edu>
2023-10-24 15:23:38 +0530

    the encoding function

--- encoder.py
+++ encoder.py
@@ -3,3 +3,18 @@
 def encoder_function(number):
     number_list = [str(int(number[i]) + 3)[-1] for i in range(len(number))]
     return ''.join(number_list)
+
+
+
+def main():
+    print('Menu')
+    print('-------------')
+    print('1. Encode')
+    print('2. Decode')
+    print('3. Quit')
+    print('')
+    option = int(input('Please enter a option: '))
+    if option == 1:
+        password = input('Please enter your password to encode: ')
+        encoded_password = encoder_function(password)
+        print('Your password has been encoded and stored!')
